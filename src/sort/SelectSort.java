54ccd6c8d42efede28f3782a80fb554db4b8f191
egbz <1023271893@qq.com>
2021-03-28 15:37:48 +0800

    select sort

---
+++ src/sort/SelectSort.java
@@ -0,0 +1,32 @@
+package sort;
+
+/**
+ * 选择排序
+ *
+ * @author egbz
+ * @date 2021/3/28
+ */
+public class SelectSort {
+    private int[] sort(int[] a) {
+        int n = a.length;
+        int tem;
+        for (int i = 0; i < n-1; i++) {
+            int k = i;
+            for (int j = i+1; j < n; j++) {
+                if (a[k] > a[j])
+                    k = j;
+            }
+            tem = a[k]; a[k] = a[i]; a[i] = tem;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {33, 12, 1, 0, 32, 5, 42, 16, 5, 75, 7, 256, 1024, 543};
+        a = new SelectSort().sort(a);
+        for (int element : a) {
+            System.out.print(element + " ");
+        }
+
+    }
+}
